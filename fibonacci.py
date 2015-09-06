b1406ebd22a200fcb73023a61e6b755e0a924f61
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-06 21:22:41 +0200

    add elegant iterative solution found online

--- fibonacci.py
+++ fibonacci.py
@@ -21,12 +21,39 @@ def fibs():
             list_.append(list_[-2] + list_[-1])
         print list_
 
-fibs()
+#fibs()
+
+###   Solution found online, seems much more elegant
+# def fibi(n):
+#     a, b = 0, 1
+#     for i in range(n):
+#         a, b = b, a + b
+#         print a
+
 
 
 # Now write another method #fibs_rec which solves 
 # the same problem recursively.
 
-#def fibs_rec(n):
+def fibs_rec(n):
+    list_ = [0, 1]
 
+    try:
+        n = int(n)
+    except ValueError, e:
+        print "You must enter a positive integer."
 
+    ## MOST IMPORTANT PART ##
+    if n <= 0:
+        print "The length must be positive integer."
+    elif n == 1 or n == 2:
+        print [i for i in range(0, n)]
+        return list_
+    else:
+        fibs_rec(n-1)
+        x = list_[-2] + list_[-1]
+        list_.append(x)
+        return list_
+
+a = fibs_rec(4)
+print a
