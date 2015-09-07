c07ccef9fc4cce6f3e7951f66e54621b7492b643
Ivan Vucetic <vuceta87@yahoo.com>
2015-09-07 17:51:26 +0200

    complete mergesort

--- mergesort.py
+++ mergesort.py
@@ -1,15 +1,26 @@
 def merge(a, b):
     list_ = []
-    for i in range(len(a if [a > b] else b)):
-        if a[i] <= b[i]:
-            list_.append(a[i])
-            list_.append(b[i])
+    # until BOTH lists are empty:
+    while (a != [] or b != []):
+        # if both have elements left, compare those:
+        if (a != [] and b != []):
+            if a[0] <= b[0]:
+                list_.append(a.pop(0))
+            else:
+                list_.append(b.pop(0))
+        
+        # once we added all from one list, add whats left from the other:
+        elif (a == [] and b != []):
+            list_.extend(b)
+            del b[:]
+        
         else:
-            list_.append(b[i])
-            list_.append(a[i])
+            list_.extend(a)
+            del a[:]
+    
     return list_
 
-# print merge(12,5)
+# print merge([12, 14, 22],[5, 18])
 
 
 def merge_sort(ary):
@@ -21,8 +32,6 @@ def merge_sort(ary):
         right = merge_sort(ary[middle:])
         return merge(left, right)
 
-a = [12, 5, 6, 1, 3, 5]
-b = [2]
 
-
+a = [12, 5, 6, 1, 3, 5, 22, 457, 88, 456, 23]
 print merge_sort(a)
